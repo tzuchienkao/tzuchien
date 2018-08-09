31a56722ba71226c1c4120a60a0ea84f15a85461
tzuchienkao <oyan114@gmail.com>
2018-08-10 01:41:10 +0800

    add design and modify json

--- libs/main.js
+++ libs/main.js
@@ -1,7 +1,7 @@
 (function($){
     $(document).ready(function(){
         $.ajax({
-            'url' : '../../tzuchien/data/demo.json', 
+            'url' : '../data/demo.json', 
             'dataType' : 'json',
             'async': false,
             success : function(content){
@@ -9,16 +9,51 @@
                     $('body').on('click', '[data-demo]', function(){
                         _co = $(this).data('demo');
                         if(_co == index){
+                            title = val.title;
+                            description = val.description;
+                            text = val.content;
+                            images = val.images;
                             $(this).popup_box({
-                                "title" : val.title, 
-                                "content" : val.content, 
+                                "title" : layout_main(title, description), 
+                                "content" : layout_content(), 
                                 "btn_close" : true, 
                                 "modal_ctrl" : true
                             })
+                            if($('.modal').is(':visible')){
+                                for(i in text) {
+                                    $('body').find('.note').append("<li>" + text[i] + "</li>")
+                                }
+                                for(j in images) {
+                                    $('body').find('.img_group').append("<li><img src='" + images[j] + "' /></li>")
+                                }
+                            }
                         }
                     })
                 })
             }
         });
+        function layout_main(title, description) {
+            return `
+                <h6>${title}</h6>
+                <p>${description}</p>
+            `
+        }
+        function layout_content() {
+            return `
+                <ul class="note"></ul>
+                <ul class="img_group"></ul>
+            `
+        }
+        function layout_text(content) {
+            return `
+                <li>${content}</li>
+            `
+        }
+        function layout_img(title, description) {
+            return `
+                <h6>${title}</h6>
+                <p>${description}</p>
+            `
+        }
     })
 })(jQuery)
